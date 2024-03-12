6015acd1708d2e66c27d4c7db985736ceb1068bb
AhshanHabib26 <habib.net27@gmail.com>
2024-03-12 09:52:41 +0600

    Single Blog Page Updated

--- src/pages/SingleBlogs.tsx
+++ src/pages/SingleBlogs.tsx
@@ -1,10 +1,25 @@
+import SingleBlogInfo from "../components/ui/SingleBlog/SingleBlogInfo";
+import Container from "../utils/Container";
 
 const SingleBlogs = () => {
-    return (
-        <div>
-            <h1>Single Blogs</h1>
+  return (
+    <div>
+      <Container>
+        <div className="grid grid-cols-1 lg:grid-cols-11 my-12">
+          <div className="lg:col-span-8 ">
+            <div className="w-full lg:w-10/12 ">
+              <SingleBlogInfo />
+            </div>
+          </div>
+          <div className="lg:col-span-3">
+            <div className="w-full border p-3 rounded-md mt-8 lg:mt-0">
+              <h1>Hello</h1>
+            </div>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default SingleBlogs;
