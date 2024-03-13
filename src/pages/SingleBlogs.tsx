7ee8435d2d73157fcd5ccfd9d373651b09be6333
AhshanHabib26 <habib.net27@gmail.com>
2024-03-13 18:14:01 +0600

    Single Blog Page Update

--- src/pages/SingleBlogs.tsx
+++ src/pages/SingleBlogs.tsx
@@ -1,5 +1,8 @@
+import BlogCategories from "../components/ui/SingleBlog/BlogCategories";
 import BlogComment from "../components/ui/SingleBlog/BlogComment";
 import BlogCommentForm from "../components/ui/SingleBlog/BlogCommentForm";
+import BlogSearchInput from "../components/ui/SingleBlog/BlogSearchInput";
+import LatestBlog from "../components/ui/SingleBlog/LatestBlog";
 import ReletedBlog from "../components/ui/SingleBlog/ReletedBlog";
 import SingleBlogInfo from "../components/ui/SingleBlog/SingleBlogInfo";
 import SingleBlogUser from "../components/ui/SingleBlog/SingleBlogUser";
@@ -10,7 +13,7 @@ const SingleBlogs = () => {
     <div>
       <Container>
         <div className="grid grid-cols-1 lg:grid-cols-11 my-12">
-          <div className="lg:col-span-8 ">
+          <div className="lg:col-span-8">
             <div className="w-full lg:w-10/12 ">
               <SingleBlogInfo />
               <SingleBlogUser />
@@ -19,9 +22,13 @@ const SingleBlogs = () => {
               <BlogCommentForm />
             </div>
           </div>
-          <div className="lg:col-span-3">
-            <div className="w-full border p-3 rounded-md mt-8 lg:mt-0">
-              <h1>Hello</h1>
+          <div className="lg:col-span-3  relative">
+            <div className=" sticky top-5">
+              <div className="w-full mt-8 lg:mt-0">
+                <BlogSearchInput />
+                <LatestBlog />
+                <BlogCategories />
+              </div>
             </div>
           </div>
         </div>
