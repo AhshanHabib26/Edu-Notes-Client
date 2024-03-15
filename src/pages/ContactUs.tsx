4e231fb7ba792977083fe1f6556b881ce6ff819c
AhshanHabib26 <habib.net27@gmail.com>
2024-03-15 09:57:38 +0600

    Contact Us Page Added

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,10 +1,18 @@
+import ContactUsInfo from "../components/ui/ContactUs/ContactUsInfo";
+import ContactUsInput from "../components/ui/ContactUs/ContactUsInput";
+import Container from "../utils/Container";
 
 const ContactUs = () => {
-    return (
-        <div>
-            <h1>Contact Us Page Cooking!</h1>
+  return (
+    <div>
+      <Container>
+        <div className="grid grid-cols-1 lg:grid-cols-2 max-w-5xl lg:mx-auto mx-5 my-12 gap-5">
+          <ContactUsInfo />
+          <ContactUsInput />
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default ContactUs;
